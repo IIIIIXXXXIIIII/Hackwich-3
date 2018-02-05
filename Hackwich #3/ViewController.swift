7ad33df9869032880c9c75cf8ddefd1f7011f60f
CM Student <cmstudent@UHWO-ACM-C-06.local>
2018-02-05 11:03:07 -1000

    -Added about icons to tab bar -Added icons to assets

--- Hackwich #3/ViewController.swift
+++ Hackwich #3/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var FirstLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+self.FirstLabel.text = "About"
+        
     }
 
     override func didReceiveMemoryWarning() {
