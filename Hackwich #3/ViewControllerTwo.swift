9ec1baebf7fb614f35425be5bbc9f58f5f750d02
CM Student <cmstudent@UHWO-ACM-C-06.local>
2018-02-05 10:20:30 -1000

    -Added 2 "Labels" -Connected UIViewController and created UIViewControllerTwo

---
+++ Hackwich #3/ViewControllerTwo.swift
@@ -0,0 +1,25 @@
+//
+//  ViewControllerTwo.swift
+//  Hackwich #3
+//
+//  Created by CM Student on 2/5/18.
+//  Copyright © 2018 CM Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
